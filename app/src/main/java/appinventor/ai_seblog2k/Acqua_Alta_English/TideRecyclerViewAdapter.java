b3cceb71412251a0eb2d06aebebc8216af2980a6
Sebastiano Tognacci <seblog2k@gmail.com>
2016-08-12 14:37:38 +0100

    formatDate method in TideRecyclerViewAdapter

--- app/src/main/java/appinventor/ai_seblog2k/Acqua_Alta_English/TideRecyclerViewAdapter.java
+++ app/src/main/java/appinventor/ai_seblog2k/Acqua_Alta_English/TideRecyclerViewAdapter.java
@@ -1,12 +1,15 @@
 package appinventor.ai_seblog2k.Acqua_Alta_English;
 
 import android.content.Context;
+import android.icu.text.SimpleDateFormat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -35,7 +38,7 @@ public class TideRecyclerViewAdapter extends RecyclerView.Adapter<TideViewHolder
     public void onBindViewHolder(TideViewHolder tideViewHolder, int position) {
         Tide tideItem = mTideList.get(position);
         Log.d(TAG, "onBindViewHolder: Processing: " + tideItem.getExtremalDate() + " position --> " + position);
-        tideViewHolder.extremalDate.setText(tideItem.getExtremalDate());
+        tideViewHolder.extremalDate.setText(formatDate(tideItem.getExtremalDate()));
         tideViewHolder.extremalType.setText(tideItem.getExtremalType());
         tideViewHolder.extremalValue.setText(tideItem.getExtremalValue());
     }
@@ -53,5 +56,22 @@ public class TideRecyclerViewAdapter extends RecyclerView.Adapter<TideViewHolder
     public void addAll(List<Tide> tideList) {
         mTideList.addAll(tideList);
         notifyDataSetChanged();
+    }
+
+    private String formatDate(String rawDate) {
+        // TODO: Sistemare con codice che prende JSON datetime e converte in data locale del tel
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date date = format.parse(rawDate);
+                SimpleDateFormat myFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                String myDateTime = myFormat.format(date);
+                Log.d(TAG, "formatDate: Formatted date: " + myDateTime);
+                return myDateTime;
+            } catch (ParseException e) {
+                e.printStackTrace();
+                Log.e(TAG, "formatDate: Error formatting date from JSON");
+            }
+        } return rawDate;
     }
 }
