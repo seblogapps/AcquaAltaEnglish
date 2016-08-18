3526f757a41e4baabd57f4385e69a25bf2dce0f7
Sebastiano Tognacci <seblog2k@gmail.com>
2016-08-18 12:35:09 +0100

    XML cleanup

--- app/src/main/java/appinventor/ai_seblog2k/Acqua_Alta_English/TideRecyclerViewAdapter.java
+++ app/src/main/java/appinventor/ai_seblog2k/Acqua_Alta_English/TideRecyclerViewAdapter.java
@@ -21,6 +21,9 @@ import java.util.List;
 public class TideRecyclerViewAdapter extends RecyclerView.Adapter<TideViewHolder> {
 
     private static final String TAG = TideRecyclerViewAdapter.class.getSimpleName();
+    private static final String JSONDateTimePattern = "yyyy-MM-dd HH:mm:ss";
+    private static final String defaultDateTimePattern = "dd/MM/yyyy HH:mm";
+
     private List<Tide> mTideList;
     private Context mContext;
 
@@ -41,7 +44,7 @@ public class TideRecyclerViewAdapter extends RecyclerView.Adapter<TideViewHolder
         Tide tideItem = mTideList.get(position);
         Log.d(TAG, "onBindViewHolder: Processing: " + tideItem.getExtremalDate() + " position --> " + position);
         tideViewHolder.extremalDate.setText(formatDate(tideItem.getExtremalDate()));
-        tideViewHolder.extremalType.setText(tideItem.getExtremalType());
+        tideViewHolder.extremalType.setText(formatExtremalType(tideItem.getExtremalType()));
         tideViewHolder.extremalValue.setText(tideItem.getExtremalValue());
     }
 
@@ -63,15 +66,15 @@ public class TideRecyclerViewAdapter extends RecyclerView.Adapter<TideViewHolder
     private String formatDate(String rawDate) {
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                SimpleDateFormat inFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                SimpleDateFormat inFormatter = new SimpleDateFormat(JSONDateTimePattern);
                 Date dateObj = inFormatter.parse(rawDate);
                 String formattedDateTime = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(dateObj);
                 Log.d(TAG, "formatDate: Formatted date (API>=24): " + formattedDateTime);
                 return formattedDateTime;
             } else {
-                java.text.SimpleDateFormat inFormatter = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                java.text.SimpleDateFormat inFormatter = new java.text.SimpleDateFormat(JSONDateTimePattern);
                 Date dateObj = inFormatter.parse(rawDate);
-                java.text.SimpleDateFormat outFormatter = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm");
+                java.text.SimpleDateFormat outFormatter = new java.text.SimpleDateFormat(defaultDateTimePattern);
                 String formattedDateTime = outFormatter.format(dateObj);
                 Log.d(TAG, "formatDate: Formatted date (API<24): " + formattedDateTime);
                 return formattedDateTime;
@@ -81,5 +84,16 @@ public class TideRecyclerViewAdapter extends RecyclerView.Adapter<TideViewHolder
             Log.e(TAG, "formatDate: Error formatting date from JSON");
             return rawDate;
         }
+    }
+
+    private String formatExtremalType(String rawExtremal) {
+        if (rawExtremal.toLowerCase().contains("min")) {
+            return "minimo";
+        } else if (rawExtremal.toLowerCase().contains("max")) {
+            return "massimo";
+        } else {
+            return rawExtremal;
+        }
+
     }
 }
